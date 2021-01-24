376eeafbc22ce1be02bdc6489568564fc818b7cf
Aditya Sinha <winadityaa@gmailcom>
2021-01-25 00:08:34 +0530

    test

---
+++ script.js
@@ -0,0 +1,17 @@
+// sidenav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "400px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0%";
+  } 
+// nav
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
